5e9f72e04053c3d57dcf1601dfef01ed6a594c22
jyler <jyler.menard@mail.concordia.ca>
2024-04-24 22:50:48 -0400

    ensure structure prediction is avaialbe

--- transvae/__init__.py
+++ transvae/__init__.py
@@ -1,5 +1,13 @@
 from __future__ import absolute_import, division, print_function
 #from .version import __version__  # noqa
-from . import data, loss, opt, rnn_models, trans_models, tvae_util
+from . import data, loss, opt, rnn_models, trans_models, tvae_util, optimization
 
-__all__ = ['data', 'loss', 'opt', 'rnn_models', 'trans_models', 'tvae_util']
+__all__ = ['data', 
+           'loss', 
+           'opt', 
+           'rnn_models', 
+           'trans_models', 
+           'tvae_util', 
+           'optimization',
+           'structure_prediction'
+]
